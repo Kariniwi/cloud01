3b28cfd3315ce14559fe7d80891bfe9175604b42
Karina <Karina>
2016-10-25 12:19:56 -0300

    se completa codigo del index

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>
 <?php
-	echo "Hola Mundo";
+	echo 'Hola Mundo';
+?>	
+</h1>
+
+<?php
+	for ($i=2; $i < 6; $i++) { 
+		# code...
+		echo "<h $i> Como estas? </h $i>";
+	}	
 ?>
+</body>
+</html>
